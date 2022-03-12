8c1f0b3f22a4cf30737cc5f12b5394ce4cd17871
saranghjaki <3340513@naver.com>
2022-03-12 15:38:47 +0900

    s

--- src/main/java/com/java/pilates/modulescode/CodeService.java
+++ src/main/java/com/java/pilates/modulescode/CodeService.java
@@ -1,19 +1,2 @@
 package com.java.pilates.modulescode;
 
-import java.util.List;
-
-public interface CodeService {
-
-	public List<Code> selectList(CodeVo vo) throws Exception; 
-	public int insert(Code dto) throws Exception;
-	public Code selectOne(CodeVo vo) throws Exception;
-	public int update(Code dto) throws Exception;
-	
-	/* CODE S */
-	public List<Code> selectListCode(CodeVo vo) throws Exception; 
-	public int insertCode(Code dto) throws Exception;
-	public Code selectOneCode(CodeVo vo) throws Exception;
-	public int updateCode(Code dto) throws Exception;
-	
-	
-}
